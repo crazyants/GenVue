863b2f99a201ca2ac97c41e58ce7b086ac846c15
Adam Adamczyk <adam@deskdoo.com>
2017-11-17 12:40:06 +0100

    Add project files.

---
+++ ViewModels/Manage/FactorViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace GenVue.ViewModels.Manage
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
---
+++ ViewModels/Manage/RemoveLoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace GenVue.ViewModels.Manage
+{
+    public class RemoveLoginViewModel
+    {
+        public string LoginProvider { get; set; }
+        public string ProviderKey { get; set; }
+    }
+}
