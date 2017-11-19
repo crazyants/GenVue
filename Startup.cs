a448c1452f1651af2c1a187435fb80f977fc7905
Adam Adamczyk <adam@deskdoo.com>
2017-11-19 19:35:04 +0100

    open ident auth req

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,11 @@ using AspNet.Security.OpenIdConnect.Primitives;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using GenVue.Services;
+using GenVue.Model.InitData;
+using System.Threading;
+using OpenIddict.Core;
+using OpenIddict.Models;
 
 namespace GenVue
 {
@@ -58,14 +63,64 @@ namespace GenVue
                 .AddEntityFrameworkStores<DefaultDbContext>()
                 .AddDefaultTokenProviders();
 
- 
-            // Register the OAuth2 validation handler.
+            // Do not 302 redirect when Unauthorized; just return 401 status code
+            // see https://www.illucit.com/blog/2016/04/asp-net-5-identity-302-redirect-vs-401-unauthorized-for-api-ajax-requests/
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Events.OnRedirectToLogin = context =>
+                {
+                    context.Response.StatusCode = 401;
+                    return Task.CompletedTask;
+                };
+            });
+
+            //services.ConfigureApplicationCookie(options =>
+            //{
+            //    options.LoginPath = new PathString("/Account/Login");
+            //    options.LogoutPath = new PathString("/Account/Logout");
+
+            //    options.Events.OnRedirectToLogin = context =>
+            //    {
+            //        if (context.Request.Path.StartsWithSegments("/api")
+            //            && context.Response.StatusCode == StatusCodes.Status200OK)
+            //        {
+            //            context.Response.Clear();
+            //            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            //            return Task.FromResult<object>(null);
+
+            //        }
+            //        context.Response.Redirect(context.RedirectUri);
+            //        return Task.FromResult<object>(null);
+            //    };
+            //});
+
+            // Configure Identity to use the same JWT claims as OpenIddict instead
+            // of the legacy WS-Federation claims it uses by default (ClaimTypes),
+            // which saves you from doing the mapping in your authorization controller.
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.ClaimsIdentity.UserNameClaimType = OpenIdConnectConstants.Claims.Name;
+                options.ClaimsIdentity.UserIdClaimType = OpenIdConnectConstants.Claims.Subject;
+                options.ClaimsIdentity.RoleClaimType = OpenIdConnectConstants.Claims.Role;
+                options.SignIn.RequireConfirmedEmail = true;
+            });
+
             services.AddAuthentication()
+                //.AddGoogle(options =>
+                //{
+                //    options.ClientId = "xxx-xx.apps.googleusercontent.com";
+                //    options.ClientSecret = "xxx-xxxx";
+                //})
+
+                //.AddTwitter(options =>
+                //{
+                //    options.ConsumerKey = "xxx";
+                //    options.ConsumerSecret = "xxx";
+                //})
+
                 .AddOAuthValidation();
 
             // Register the OpenIddict services.
-            // Note: use the generic overload if you need
-            // to replace the default OpenIddict entities.
             services.AddOpenIddict(options =>
             {
                 // Register the Entity Framework stores.
@@ -76,19 +131,54 @@ namespace GenVue
                 // bind OpenIdConnectRequest or OpenIdConnectResponse parameters.
                 options.AddMvcBinders();
 
-                // Enable the token endpoint (required to use the password flow).
-                options.EnableTokenEndpoint("/api/auth/login");
+                // Enable the authorization, logout, token and userinfo endpoints.
+                options.EnableAuthorizationEndpoint("/api/connect/authorize")
+                       .EnableLogoutEndpoint("/api/connect/logout")
+                       .EnableTokenEndpoint("/api/connect/token")
+                       .EnableUserinfoEndpoint("/api/userinfo");
 
-                // Allow client applications to use the grant_type=password flow.
-                options.AllowPasswordFlow();
+                // Note: the Mvc.Client sample only uses the code flow and the password flow, but you
+                // can enable the other flows if you need to support implicit or client credentials.
+                options.AllowAuthorizationCodeFlow()
+                       .AllowPasswordFlow()
+                       .AllowRefreshTokenFlow();
+
+                // Mark the "profile" scope as a supported scope in the discovery document.
+                options.RegisterScopes(OpenIdConnectConstants.Scopes.Profile);
+
+                // Make the "client_id" parameter mandatory when sending a token request.
+                options.RequireClientIdentification();
+
+                // When request caching is enabled, authorization and logout requests
+                // are stored in the distributed cache by OpenIddict and the user agent
+                // is redirected to the same page with a single parameter (request_id).
+                // This allows flowing large OpenID Connect requests even when using
+                // an external authentication provider like Google, Facebook or Twitter.
+                options.EnableRequestCaching();
 
                 // During development, you can disable the HTTPS requirement.
                 options.DisableHttpsRequirement();
+
+                // Note: to use JWT access tokens instead of the default
+                // encrypted format, the following lines are required:
+                //
+                // options.UseJsonWebTokens();
+                // options.AddEphemeralSigningKey();
             });
+
+            services.AddTransient<IEmailSender, AuthMessageSender>();
+            services.AddTransient<ISmsSender, AuthMessageSender>();
+
+            // Add framework services
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
@@ -110,6 +200,16 @@ namespace GenVue
 
             app.UseAuthentication();
 
+            // Seed the database with the sample applications.
+            // Note: in a real world application, this step should be part of a setup script.
+            InitializeAsync(app.ApplicationServices, CancellationToken.None).GetAwaiter().GetResult();
+
+            if (env.IsDevelopment())
+            {
+                // init roles and users
+                DBInitilizer.Initialize(serviceProvider);
+            }
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
@@ -120,6 +220,53 @@ namespace GenVue
                     name: "spa-fallback",
                     defaults: new { controller = "Home", action = "Index" });
             });
+        }
+
+        private async Task InitializeAsync(IServiceProvider services, CancellationToken cancellationToken)
+        {
+            // Create a new service scope to ensure the database context is correctly disposed when this methods returns.
+            using (var scope = services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<DefaultDbContext>();
+                await context.Database.EnsureCreatedAsync();
+
+                var manager = scope.ServiceProvider.GetRequiredService<OpenIddictApplicationManager<OpenIddictApplication>>();
+
+                if (await manager.FindByClientIdAsync("mvc", cancellationToken) == null)
+                {
+                    var descriptor = new OpenIddictApplicationDescriptor
+                    {
+                        ClientId = "mvc",
+                        ClientSecret = "101564A5-E7FE-42CB-B10D-61EF6A8F3651",
+                        DisplayName = "MVC client application",
+                        PostLogoutRedirectUris = { new Uri("http://localhost:57496/signout-callback-oidc") },
+                        RedirectUris = { new Uri("http://localhost:57496/signin-oidc") }
+                    };
+
+                    await manager.CreateAsync(descriptor, cancellationToken);
+                }
+
+                // To test this sample with Postman, use the following settings:
+                //
+                // * Authorization URL: http://localhost:57496/connect/authorize
+                // * Access token URL: http://localhost:57496/connect/token
+                // * Client ID: postman
+                // * Client secret: [blank] (not used with public clients)
+                // * Scope: openid email profile roles
+                // * Grant type: authorization code
+                // * Request access token locally: yes
+                if (await manager.FindByClientIdAsync("postman", cancellationToken) == null)
+                {
+                    var descriptor = new OpenIddictApplicationDescriptor
+                    {
+                        ClientId = "postman",
+                        DisplayName = "Postman",
+                        RedirectUris = { new Uri("https://www.getpostman.com/oauth2/callback") }
+                    };
+
+                    await manager.CreateAsync(descriptor, cancellationToken);
+                }
+            }
         }
     }
 }
